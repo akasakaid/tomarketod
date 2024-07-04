34a626314be40f1d5b4ce2a320df200ddb70d346
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2024-07-04 20:07:18 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,277 @@
+import os
+import sys
+import json
+import time
+import random
+import argparse
+import requests
+from base64 import b64decode
+from datetime import datetime
+from urllib.parse import parse_qs
+from colorama import init, Fore, Style
+
+merah = Fore.LIGHTRED_EX
+kuning = Fore.LIGHTYELLOW_EX
+hijau = Fore.LIGHTGREEN_EX
+biru = Fore.LIGHTBLUE_EX
+putih = Fore.LIGHTWHITE_EX
+hitam = Fore.LIGHTBLACK_EX
+reset = Style.RESET_ALL
+line = putih + "~" * 50
+
+
+class Tomartod:
+    def __init__(self):
+        self.headers = {
+            "host": "api-web.tomarket.ai",
+            "connection": "keep-alive",
+            "accept": "application/json, text/plain, */*",
+            "user-agent": "Mozilla/5.0 (Linux; Android 10; Redmi 4A / 5A Build/QQ3A.200805.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36",
+            "content-type": "application/json",
+            "origin": "https://mini-app.tomarket.ai",
+            "x-requested-with": "tw.nekomimi.nekogram",
+            "sec-fetch-site": "same-site",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-dest": "empty",
+            "referer": "https://mini-app.tomarket.ai/",
+            "accept-language": "en-US,en;q=0.9",
+        }
+        self.marinkitagawa = lambda data: {
+            key: value[0] for key, value in parse_qs(data).items()
+        }
+
+    def set_authorization(self, auth):
+        self.headers["authorization"] = auth
+
+    def del_authorization(self):
+        if "authorization" in self.headers.keys():
+            self.headers.pop("authorization")
+
+    def login(self, data):
+        url = "https://api-web.tomarket.ai/tomarket-game/v1/user/login"
+        data = json.dumps(
+            {
+                "init_data": data,
+                "invite_code": "",
+            }
+        )
+        self.del_authorization()
+        res = self.http(url, self.headers, data)
+        if res.status_code != 200:
+            self.log(f"{merah}failed fetch token authorization, check http.log !")
+            return None
+        token = res.json()["data"]["access_token"]
+        return token
+
+    def start_farming(self):
+        data = json.dumps({"game_id": "53b22103-c7ff-413d-bc63-20f6fb806a07"})
+        url = "https://api-web.tomarket.ai/tomarket-game/v1/farm/start"
+        res = self.http(url, self.headers, data)
+        if res.status_code != 200:
+            self.log(f"{merah}failed start farming, check http.log last line !")
+            return False
+
+        data = res.json().get("data")
+        end_farming = data["end_at"]
+        format_end_farming = (
+            datetime.fromtimestamp(end_farming).isoformat(" ").split(".")[0]
+        )
+        self.log(f"{hijau}success start farming !")
+
+    def end_farming(self):
+        data = json.dumps({"game_id": "53b22103-c7ff-413d-bc63-20f6fb806a07"})
+        url = "https://api-web.tomarket.ai/tomarket-game/v1/farm/claim"
+        res = self.http(url, self.headers, data)
+        if res.status_code != 200:
+            self.log(f"{merah}failed start farming, check http.log last line !")
+            return False
+
+        poin = res.json()["data"]["claim_this_time"]
+        self.log(f"{hijau}success claim farming !")
+        self.log(f"{hijau}reward : {putih}{poin}")
+
+    def daily_claim(self):
+        url = "https://api-web.tomarket.ai/tomarket-game/v1/daily/claim"
+        data = json.dumps({"game_id": "fa873d13-d831-4d6f-8aee-9cff7a1d0db1"})
+        res = self.http(url, self.headers, data)
+        if res.status_code != 200:
+            self.log(f"{merah}failed claim daily sign,check http.log last line !")
+            return False
+
+        self.log(f"{hijau}success claim daily sign !")
+        data = res.json().get("data")
+        poin = data["today_points"]
+        self.log(f"{hijau}reward : {putih}{poin}")
+        return
+
+    def get_balance(self):
+        url = "https://api-web.tomarket.ai/tomarket-game/v1/user/balance"
+        while True:
+            res = self.http(url, self.headers, "")
+            data = res.json().get("data")
+            if data is None:
+                self.log(f"{merah}failed get data !")
+                return None
+
+            timestamp = data["timestamp"]
+            balance = data["available_balance"]
+            self.log(f"{hijau}balance : {putih}{balance}")
+            if "daily" not in data.keys():
+                self.daily_claim()
+
+            if "farming" not in data.keys():
+                self.log(f"{kuning}farming not started !")
+                result = self.start_farming()
+                continue
+
+            end_farming = data["farming"]["end_at"]
+            format_end_farming = (
+                datetime.fromtimestamp(end_farming).isoformat(" ").split(".")[0]
+            )
+            if timestamp > end_farming:
+                self.end_farming()
+                continue
+
+            self.log(f"{kuning}not time to claim !")
+            self.log(f"{kuning}end farming at : {putih}{format_end_farming}")
+            _next = end_farming - timestamp
+            return _next
+
+    def load_data(self, file):
+        datas = open(file).read().splitlines()
+        if len(datas) <= 0:
+            print(
+                f"{merah}0 account detected from {file}, fill your data in {file} first !{reset}"
+            )
+            sys.exit()
+
+        return datas
+
+    def load_config(self, file):
+        config = json.loads(open(file).read())
+        self.interval = config["interval"]
+        self.play_game = config["play_game"]
+        self.game_low_point = config["game_point"]["low"]
+        self.game_high_point = config["game_point"]["high"]
+
+    def save(self, id, token):
+        tokens = json.loads(open("tokens.json").read())
+        tokens[str(id)] = token
+        open("tokens.json", "w").write(json.dumps(tokens, indent=4))
+
+    def get(self, id):
+        tokens = json.loads(open("tokens.json").read())
+        if str(id) not in tokens.keys():
+            return None
+
+        return tokens[str(id)]
+
+    def is_expired(self, token):
+        header, payload, sign = token.split(".")
+        deload = b64decode(payload + "==").decode()
+        jeload = json.loads(deload)
+        now = int(datetime.now().timestamp())
+        if now > jeload["exp"]:
+            return True
+        return False
+
+    def http(self, url, headers, data=None):
+        while True:
+            try:
+                now = datetime.now().isoformat(" ").split(".")[0]
+                if data is None:
+                    res = requests.get(url, headers=headers)
+                    open("http.log", "a").write(
+                        f"{now} - {res.status_code} - {res.text}\n"
+                    )
+                    return res
+
+                if data == "":
+                    res = requests.post(url, headers=headers)
+                    open("http.log", "a").write(
+                        f"{now} - {res.status_code} - {res.text}\n"
+                    )
+                    return res
+
+                res = requests.post(url, headers=headers, data=data)
+                open("http.log", "a").write(f"{now} - {res.status_code} - {res.text}\n")
+                return res
+            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+                print(f"{merah}connection error / connection timeout !")
+                time.sleep(1)
+                continue
+
+    def countdown(self, t):
+        while t:
+            menit, detik = divmod(t, 60)
+            jam, menit = divmod(menit, 60)
+            jam = str(jam).zfill(2)
+            menit = str(menit).zfill(2)
+            detik = str(detik).zfill(2)
+            print(f"{putih}waiting {jam}:{menit}:{detik}     ", flush=True, end="\r")
+            time.sleep(1)
+            t -= 1
+        print("                                        ", flush=True, end="\r")
+
+    def log(self, msg):
+        now = datetime.now().isoformat(" ").split(".")[0]
+        print(f"{hitam}[{now}]{reset} {msg}{reset}")
+
+    def main(self):
+        banner = f"""
+    {hijau}Auto Claim {biru}Tomarket_ai
+    
+    {hijau}By: {putih}t.me/AkasakaID
+    {hijau}GIthub: {putih}@AkasakaID
+    
+    {hijau}Message: {putih}dont't forget to 'git pull' maybe the script is updated 
+    
+        """
+        arg = argparse.ArgumentParser()
+        arg.add_argument("--data", default="data.txt")
+        arg.add_argument("--config", default="config.json")
+        arg.add_argument("--marinkitagawa", action="store_true")
+        args = arg.parse_args()
+        if not args.marinkitagawa:
+            os.system("cls" if os.name == "nt" else "clear")
+        print(banner)
+        self.load_config(args.config)
+        datas = self.load_data(args.data)
+        self.log(f"{biru}total account : {putih}{len(datas)}")
+        print(line)
+        while True:
+            list_countdown = []
+            _start = int(time.time())
+            for no, data in enumerate(datas):
+                parser = self.marinkitagawa(data)
+                user = json.loads(parser["user"])
+                id = user["id"]
+                now = datetime.now().isoformat(" ").split(".")[0]
+                self.log(
+                    f"{hijau}account number : {putih}{no+1}{hijau}/{putih}{len(datas)}"
+                )
+                self.log(f"{hijau}name : {putih}{user['first_name']}")
+                token = self.get(id)
+                if token is None:
+                    token = self.login(data)
+                    self.save(id, token)
+                if self.is_expired(token):
+                    token = self.login(data)
+                    self.save(id, token)
+                self.set_authorization(token)
+                result = self.get_balance()
+                print(line)
+                self.countdown(self.interval)
+                list_countdown.append(result)
+            _end = int(time.time())
+            _tot = _end - _start
+            _min = min(list_countdown) - _tot
+            self.countdown(_min)
+
+
+if __name__ == "__main__":
+    try:
+        app = Tomartod()
+        app.main()
+    except KeyboardInterrupt:
+        sys.exit()
